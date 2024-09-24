839af4e0c9a2575f4db7a4864d7d32577a47f1a7
Mccheese178 <137913700+Mccheese178@users.noreply.github.com>
2024-09-24 13:59:56 -0400

    Update main.py

--- main.py
+++ main.py
@@ -11,7 +11,7 @@ username = "" # Your username on the PiShock website
 api_key = "" # Your api key from the PiShock website
 share_code = "" # Code that you want the alarm to use 
 app_name = "" # What you want to name the app
-test_mode = False  # Set to true if you want to bypass setting an alarm
+test_mode = False  # Set to 'True' if you want to bypass setting an alarm
 snooze_duration = "" # Set in minutes how long you want a snooze. If you don't want a snooze, set to '0'
 
 # DO NOT CHANGE ANYTHING BELOW OR THE CODE WILL NOT RUN PROPERLY
